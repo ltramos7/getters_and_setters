5c026b60a02da74e371a6ce6554fd0a571fbdfdb
Linda <ltramos7@gmail.com>
2020-10-12 11:14:15 -0600

    added a setter method for major.

--- student.rb
+++ student.rb
@@ -10,13 +10,17 @@ class Student
     end
 
      # A getter method for major.
-     def major
+    def major
         @major
+    end
+
+    # A setter method for major.
+    def set_major=(major)
+        @major = major
     end
 end
 
 student = Student.new("John", "Math")
-puts student.name
 puts student.major
 
 
