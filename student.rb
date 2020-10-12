7cb3db796f5f2d5f0cedb792c6e350868101f82a
Linda <ltramos7@gmail.com>
2020-10-12 09:58:54 -0600

    created a Student class with an initializer method and setter methods.

---
+++ student.rb
@@ -0,0 +1,24 @@
+class Student
+    def initialize(name, major)
+        @name = name
+        @major = major
+    end
+
+    # These are setter methods.
+    def set_name=(name)
+        @name = name
+    end
+
+    def set_major=(major)
+        @major = major
+    end
+
+end
+
+
+
+
+
+
+
+
